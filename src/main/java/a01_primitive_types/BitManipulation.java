a24e92c694e53d313b067a54a6e455ca2c7b3ba8
Liqun Chen <Liqun.Chen@Priceline.com>
2018-02-18 06:25:15 -0500

    more

--- src/main/java/a01_primitive_types/BitManipulation.java
+++ src/main/java/a01_primitive_types/BitManipulation.java
@@ -115,6 +115,102 @@ public class BitManipulation {
 		return result;
 	}
 
+	public static long multiply(long x, long y) {
+		long sum = 0;
+
+		while (x != 0) {
+			if ((x & 1) != 0) {
+				sum = add(sum, y);
+			}
+			x >>>= 1;
+			y <<= 1;
+		}
+
+		return sum;
+	}
+
+	private static long add(long a, long b) {
+		long sum = 0, carryin = 0, k = 1, tempA = a, tempB = b;
+		while (tempA != 0 || tempB != 0) {
+			long ak = a & k, bk = b & k;
+			long carryout = (ak & bk) | (ak & carryin) | (bk & carryin);
+			sum |= (ak ^ bk ^ carryin);
+			carryin = carryout << 1;
+			k <<= 1;
+			tempA >>>= 1;
+			tempB >>>= 1;
+		}
+		return sum | carryin;
+	}
+
+	/**
+	 * Given two positive integers, compute their quotient, using only the addition, subtraction,
+	 * and shifting operators.
+	 */
+	public static int divide(int x, int y) {
+		if (y == 0)
+			throw new IllegalArgumentException();
+		int result = 0;
+		int power = 32;
+		long yPower = y << power;
+		while (x >= y) {
+			while (yPower > x) {
+				yPower >>>= 1;
+				power--;
+			}
+
+			result += 1 << power;
+			x -= yPower;
+		}
+		return result;
+	}
+
+	/**
+	 * Write a program that takes a double x and an integer y and returns $$x^y$$. You can ignore
+	 * overflow and underflow.
+	 */
+
+	// bottom-up recursive
+	public static double power(double x, int y) {
+		double result = 1.0;
+
+		if (y < 0) {
+			y = -y;
+			x = 1.0 / x;
+		}
+
+		while (y != 0) {
+			if ((y & 1) == 1) // will run twice for odd number, and only once for even number!
+				result *= x;
+			x *= x;
+			y >>>= 1;
+		}
+
+		return result;
+	}
+
+	// top-down recursive
+	public static double powerII(double x, int y) {
+		if (y == 0)
+			return 1;
+		if (y == 1)
+			return x;
+
+		if (y < 0) {
+			y = -y;
+			x = 1.0 / x;
+		}
+
+		int s = y >> 1; // divide by 2
+		double half = powerII(x, s);
+
+		if ((y & 1) == 0)
+			return half * half;
+		else
+			return half * half * x;
+
+	}
+
 	/**
 	 * Write a program which takes an integer and returns the integer corresponding to the digits of
 	 * the input written in reverse order. For example, the reverse of 42 is 24, and the reverse of
@@ -197,6 +293,11 @@ public class BitManipulation {
 		assert isPalindromeNumber(2147447412) == true;
 		assert reverseDigits(-314) == -413;
 		assert uniformRandom(1, 6) >= 1 && uniformRandom(1, 6) <= 6;
+		assert power(2.0, 3) == powerII(2.0, 3);
+		assert power(2.0, 4) == powerII(2.0, 4);
+		assert power(2.0, -2) == powerII(2.0, -2);
+		assert divide(10, 2) == 5;
+		assert multiply(2, 3) == 6;
 	}
 
 }
