a5664414259246ee2749b121e8357bfd826d6e8b
lchen676 <lchen676@bloomberg.net>
2021-09-30 07:34:00 -0400

    Refine

--- src/main/java/a02_arrays_strings/CountRangeSum.java
+++ src/main/java/a02_arrays_strings/CountRangeSum.java
@@ -26,19 +26,20 @@ public class CountRangeSum {
     return divide(sums, new long[sums.length], 0, sums.length - 1, lower, upper);
   }
 
-  public int divide(long[] sums, long[] temp, int high, int low, int lower, int upper) {
-    if (high >= low)
+  public int divide(long[] sums, long[] temp, int low, int high, int lower, int upper) {
+    if (low >= high)
       return 0;
 
-    int count = 0, mid = (high + low + 1) / 2;
-    count += divide(sums, temp, high, mid - 1, lower, upper);
-    count += divide(sums, temp, mid, low, lower, upper);
-    count += merge(sums, temp, high, mid, low, lower, upper);
+    int count = 0, mid = (low + high + 1) / 2;
+    count += divide(sums, temp, low, mid - 1, lower, upper);
+    count += divide(sums, temp, mid, high, lower, upper);
+    count += merge(sums, temp, low, mid, high, lower, upper);
     return count;
   }
 
   public int merge(long[] sums, long[] temp, int low, int mid, int high, int lower, int upper) {
-    // compare and count
+    // low~mid and mid+1~high are both sorted at this point
+    // Scan the left half and pair with right half
     int count = 0, start = mid, end = mid;
     for (int i = low; i < mid; i++) {
       while (start <= high && sums[start] - sums[i] < lower)
@@ -57,22 +58,17 @@ public class CountRangeSum {
         temp[current++] = sums[right++];
       }
     }
-    // cache rest of left
+    // copy the rest of left half
     while (left < mid) {
       temp[current++] = sums[left++];
     }
-    // cach rest of right
+    // copy the rest of right half
     while (right <= high) {
       temp[current++] = sums[right++];
     }
 
-    // copy all sorted sums
-    int length = high - low + 1;
-    for (int j = 0; j < length; j++) {
-      sums[low + j] = temp[low + j];
-    }
-    // or use array copy!
-    // System.arraycopy(temp, low, sums, low, high - low + 1);
+    // copy sorted temp (low~high) to sums
+    System.arraycopy(temp, low, sums, low, high - low + 1);
 
     return count;
   }
